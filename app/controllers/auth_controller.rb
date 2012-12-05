d69b443bb9204b185b9ac7b2e4cc2dfaa1a22ca6
Raghu Betina <rbetina@gmail.com>
2012-12-05 00:15:37 -0600

    Completed Step 6 of Login for Server-side Apps documentation.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,16 @@
 class AuthController < ApplicationController
   def facebook
     if code = params[:code]
+      facebook_app_id = '132081450279923'
+      facebook_app_secret = 'a0ed5f078dd9ab4306bf081bdf335d7c'
+      url = "https://graph.facebook.com/oauth/access_token?client_id=#{facebook_app_id}&redirect_uri=http://localhost:3000/auth/facebook&client_secret=#{facebook_app_secret}&code=#{code}"
+      
+      response = open(url).read
+      access_token = response.split('&').first.split('=').last
+            
+      @current_user.facebook_access_token = access_token            
+      @current_user.save
+      
       redirect_to root_url, :notice => 'Facebook authorization successful.'
     else
       redirect_to root_url, :notice => 'Facebook authorization unsuccessful. Please try again.'
