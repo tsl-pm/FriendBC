3ab266b346def5109217f4fddaaaab345a291d27
Raghu Betina <rbetina@gmail.com>
2012-12-05 02:01:06 -0600

    Filtering posts by type video.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,8 @@ class PagesController < ApplicationController
     if @current_user
       url = "https://graph.facebook.com/me/home?access_token=#{@current_user.facebook_access_token}&limit=200"
       response = open(url).read
-      @posts = JSON.parse(response)["data"]
+      posts = JSON.parse(response)["data"]
+      @posts_with_videos = posts.select { |p| p["type"] == "video" && p["link"].present? }
     else
       redirect_to new_session_url, 'Please sign in.'
     end
