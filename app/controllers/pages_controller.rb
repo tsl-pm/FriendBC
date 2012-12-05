898fcae233920b550ae99db35411f18fe3292364
Raghu Betina <rbetina@gmail.com>
2012-12-05 01:57:31 -0600

    Pulling home feed from Facebook.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
+require 'open-uri'
+
 class PagesController < ApplicationController
   def home
+    if @current_user
+      url = "https://graph.facebook.com/me/home?access_token=#{@current_user.facebook_access_token}&limit=200"
+      response = open(url).read
+      @posts = JSON.parse(response)["data"]
+    else
+      redirect_to new_session_url, 'Please sign in.'
+    end
   end
 end
