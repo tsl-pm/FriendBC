376685bca6325e7469ab2ba9bcd5aaa93ecd1c9d
Raghu Betina <rbetina@gmail.com>
2012-12-04 23:39:23 -0600

    Secured User sign-in, added header.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @user = User.find_by_email(params[:email])
+    
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_url, :notice => 'Sign-in successful.'
+    else
+      flash[:notice] = 'Sign-in unsuccessful. Please try again.'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    reset_session
+    redirect_to root_url, :notice => 'Sign-out successful.'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 FriendBC::Application.routes.draw do
   resources :users
 
+  resource :session, :only => [:new, :create, :destroy]
+
   get "pages/home"
 
   root :to => 'Pages#home'
